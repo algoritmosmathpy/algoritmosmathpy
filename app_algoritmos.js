814c1249436f4b17f5c87c483cff128295e43f97
algoritmosmathpy <30539598+algoritmosmathpy@users.noreply.github.com>
2017-08-02 18:44:45 -0400

    Update app_algoritmos.js

--- app_algoritmos.js
+++ app_algoritmos.js
@@ -9,6 +9,7 @@ angular.module('aplicacionAlgoritmos', []).controller("controlador", function($s
 	var ref = new firebase.database().ref("algoritmos");
 		ref.on("child_added", function(snapshot, prevChildKey){
   		var datos = snapshot.val();
+		$('#p2').removeClass('mdl-progress mdl-js-progress mdl-progress__indeterminate');
   		$scope.$apply(function(){
   			$scope.algoritmo.push({name: datos.nombre, info: datos.descripcion, url: datos.url, img: datos.img, ancle: datos.ancle, datePublic: datos.date});
   		})
