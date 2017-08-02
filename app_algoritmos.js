019e42b7cbb15a0ff088a754ca0816af3e626794
algoritmosmathpy <30539598+algoritmosmathpy@users.noreply.github.com>
2017-08-02 18:33:58 -0400

    Add files via upload

---
+++ app_algoritmos.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+  $('#card_welcome').fadeIn(2000);
+}
+
+angular.module('aplicacionAlgoritmos', []).controller("controlador", function($scope){
+	$scope.algoritmo = [
+		
+	];
+	var ref = new firebase.database().ref("algoritmos");
+		ref.on("child_added", function(snapshot, prevChildKey){
+  		var datos = snapshot.val();
+  		$scope.$apply(function(){
+  			$scope.algoritmo.push({name: datos.nombre, info: datos.descripcion, url: datos.url, img: datos.img, ancle: datos.ancle, datePublic: datos.date});
+  		})
+  	});
+});
